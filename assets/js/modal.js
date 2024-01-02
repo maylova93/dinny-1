9afb9d62ac48cf089921175d6309b62d52112bac
bo-nicolaisen <bni@techcollege.dk>
2024-01-02 11:04:52 +0100

    prep work

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -6,15 +6,19 @@ displayModal("Cookie_container")
 
 function displayModal(id) {
     document.getElementById(id).style.display = "block";
+    document.getElementById("myBody").classList.toggle("noScroll");
+
 }
 
 
 function displayGallery(id, src, desc) {
     document.getElementById(id).style.display = "block";
     document.getElementById("desc").innerHTML = desc;
+    document.getElementById("myBody").classList.toggle("noScroll");
 }
 
 
 function closeModal(id) {
     document.getElementById(id).style.display = "none";
+    document.getElementById("myBody").classList.toggle("noScroll");
 }
